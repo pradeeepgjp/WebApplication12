b05f5ad1cd340f101520607289c4b18a09317d55
hp <hp@LAPTOP-8L733U4H>
2023-11-08 00:53:04 +0530

    Add project files.

---
+++ Controllers/HeroesController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication12.Data;
+using WebApplication12.Models;
+
+namespace WebApplication12.Controllers
+{
+    [Authorize]
+    public class HeroesController : Controller
+    {
+        private readonly HeroDbContext _context;
+
+        public HeroesController(HeroDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Heroes
+        public async Task<IActionResult> Index()
+        {
+              return _context.HeroList != null ? 
+                          View(await _context.HeroList.ToListAsync()) :
+                          Problem("Entity set 'HeroDbContext.HeroList'  is null.");
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Furniture()
+        {
+            return View();
+        }
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        // GET: Heroes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.HeroList == null)
+            {
+                return NotFound();
+            }
+
+            var hero = await _context.HeroList
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hero == null)
+            {
+                return NotFound();
+            }
+
+            return View(hero);
+        }
+
+        // GET: Heroes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Heroes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Mobile,Email,Source")] Hero hero)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hero);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hero);
+        }
+
+        // GET: Heroes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.HeroList == null)
+            {
+                return NotFound();
+            }
+
+            var hero = await _context.HeroList.FindAsync(id);
+            if (hero == null)
+            {
+                return NotFound();
+            }
+            return View(hero);
+        }
+
+        // POST: Heroes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Mobile,Email,Source")] Hero hero)
+        {
+            if (id != hero.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hero);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HeroExists(hero.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(hero);
+        }
+
+        // GET: Heroes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.HeroList == null)
+            {
+                return NotFound();
+            }
+
+            var hero = await _context.HeroList
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hero == null)
+            {
+                return NotFound();
+            }
+
+            return View(hero);
+        }
+
+        // POST: Heroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.HeroList == null)
+            {
+                return Problem("Entity set 'HeroDbContext.HeroList'  is null.");
+            }
+            var hero = await _context.HeroList.FindAsync(id);
+            if (hero != null)
+            {
+                _context.HeroList.Remove(hero);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HeroExists(int id)
+        {
+          return (_context.HeroList?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Data/HeroDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication12.Models;
+
+namespace WebApplication12.Data
+{
+    public class HeroDbContext : IdentityDbContext
+    {
+        public HeroDbContext(DbContextOptions<HeroDbContext> options) : base(options) { }
+
+        public DbSet<Hero> HeroList { get; set; }
+
+    }
+}
